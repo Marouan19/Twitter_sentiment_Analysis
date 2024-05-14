44f9cc414ab7436e4a3ceef9bcd032b94a17b782
marouan19 <marwandaghmoumi2017@gmail.com>
2024-05-14 17:05:43 +0100

    consumer work

--- consumer/consume.py
+++ consumer/consume.py
@@ -1,11 +1,6 @@
 from kafka import KafkaConsumer
 from json import loads
 from pymongo import MongoClient
-from flask_socketio import SocketIO, send
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
-socketio = SocketIO(app)
 
 # Kafka broker address
 bootstrap_servers = ['localhost:9092']
@@ -40,16 +35,11 @@ def save_to_mongodb():
                 }
                 collection.insert_one(tweet_dict)
                 print(f"Saved tweet to MongoDB: {tweet_dict}")
-                # Emit the received data to the client side
-                socketio.emit('tweet_data', tweet_dict)
             else:
                 print(f"Received message is not in the expected format: {tweet_data}")
         except Exception as e:
             print(f"Error processing message: {e}")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    save_to_mongodb()
